a4ec02afc400855c90e485fb552f3174183cc583
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 15:10:07 +0530

    initial commit

---
+++ DumpClassicVMs.ps1
@@ -0,0 +1,53 @@
+﻿param(
+    [string]$tenantId="", 
+    [string]$file="Azure-Classic-VMs.csv"
+) 
+
+if ($tenantId -eq "") {
+    Write-Host "No tenant specified."
+    Add-AzureAccount
+} else {
+    add-azureaccount -Tenant $tenantId
+}
+
+$subs = Get-AzureSubscription 
+$vmobjs = @()
+
+foreach ($sub in $subs | where-object { ($_.TenantId -eq $tenantId) -or ($tenantId -eq "") } )
+{
+    Write-Host Processing subscription $sub.SubscriptionName
+
+    try
+    {
+        Select-AzureSubscription -SubscriptionId $sub.SubscriptionId -ErrorAction Continue
+    
+        $vms = Get-AzureVm
+        $svcs = Get-AzureService
+
+        foreach ($vm in $vms)
+        {
+            $service = $svcs | where-object { $_.ServiceName -eq $vm.ServiceName }
+
+            $vmInfo = [pscustomobject]@{
+                'Subscription'=$sub.SubscriptionName
+                'Mode'='Classic'
+                'Name'=$vm.Name
+                'ServiceName' = $vm.ServiceName
+                'Location' = $service.Location
+                'VMSize' = $vm.InstanceSize
+                'Status' = $vm.Status
+                'AvailabilitySet' = $vm.AvailabilitySetName}
+        
+            $vmobjs += $vmInfo
+
+        }  
+    }
+    catch
+    {
+        Write-Host $error[0]
+    }
+}
+
+
+$vmobjs | Export-Csv -Path $file
+Write-Host "VM list written to $file"
